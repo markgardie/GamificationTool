5775aace1e1881a853f9ce2816b862fe9845057a
markgardie <60879509+markgardie@users.noreply.github.com>
2024-07-09 17:17:18 +0300

    initial commit

---
+++ src/main/kotlin/com/kpi/gamificationtool/GamificationToolApplication.kt
@@ -0,0 +1,11 @@
+package com.kpi.gamificationtool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamificationToolApplication
+
+fun main(args: Array<String>) {
+    runApplication<GamificationToolApplication>(*args)
+}
---
+++ src/test/kotlin/com/kpi/gamificationtool/GamificationToolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kpi.gamificationtool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamificationToolApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
