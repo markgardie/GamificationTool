4ea502d96af909af0921eed24f2f668147340c45
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-15 16:30:34 +0200

    add styles to game rules pages

---
+++ src/main/resources/static/js/game-rules.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.add-rule-btn').addEventListener('click', function() {
+    const form = document.querySelector('.add-rule-form');
+    form.style.display = form.style.display === 'none' ? 'block' : 'none';
+});
