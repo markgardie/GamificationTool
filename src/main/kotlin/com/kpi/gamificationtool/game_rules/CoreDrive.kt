534805d608cd7af24383e11f3c7c252f9ad5b1c8
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-15 17:03:31 +0200

    add Ukrainian variants of Core Drives

--- src/main/kotlin/com/kpi/gamificationtool/game_rules/CoreDrive.kt
+++ src/main/kotlin/com/kpi/gamificationtool/game_rules/CoreDrive.kt
@@ -1,12 +1,12 @@
 package com.kpi.gamificationtool.game_rules
 
-enum class CoreDrive {
-    MEANING,
-    ACCOMPLISHMENT,
-    CREATIVITY,
-    OWNERSHIP,
-    SOCIAL,
-    SCARCITY,
-    UNPREDICTABILITY,
-    AVOIDANCE
+enum class CoreDrive(val ukName: String) {
+    MEANING("Сенс"),
+    ACCOMPLISHMENT("Досягнення"),
+    CREATIVITY("Креативність"),
+    OWNERSHIP("Володіння"),
+    SOCIAL("Соціальний вплив"),
+    SCARCITY("Дефіцит"),
+    UNPREDICTABILITY("Непередбачуваність"),
+    AVOIDANCE("Уникнення")
 }
