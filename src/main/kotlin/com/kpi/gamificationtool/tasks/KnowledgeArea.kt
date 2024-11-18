843125f73d190548947918e7283512c7719c80cf
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-18 19:37:34 +0200

    add models for tasks

---
+++ src/main/kotlin/com/kpi/gamificationtool/tasks/KnowledgeArea.kt
@@ -0,0 +1,14 @@
+package com.kpi.gamificationtool.tasks
+
+enum class KnowledgeArea(val ukName: String) {
+    SOFTWARE_REQUIREMENTS("Вимоги до програмного забезпечення"),
+    SOFTWARE_DESIGN("Проєктування програмного забезпечення"),
+    SOFTWARE_CONSTRUCTION("Конструювання програмного забезпечення"),
+    SOFTWARE_TESTING("Тестування програмного забезпечення"),
+    SOFTWARE_MAINTENANCE("Супровід програмного забезпечення"),
+    SOFTWARE_CONFIGURATION_MANAGEMENT("Керування конфігурацією"),
+    SOFTWARE_ENGINEERING_MANAGEMENT("Керування програмною інженерією"),
+    SOFTWARE_QUALITY("Якість програмного забезпечення"),
+    SOFTWARE_ENGINEERING_PROFESSIONAL_PRACTICE("Професійна практика програмної інженерії"),
+
+}
