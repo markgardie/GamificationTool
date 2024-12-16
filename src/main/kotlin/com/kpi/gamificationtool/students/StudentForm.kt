340a37231c82f57399b510fb46212789c6cf7646
markgardie <60879509+markgardie@users.noreply.github.com>
2024-12-16 17:36:32 +0200

    change age to birth date

--- src/main/kotlin/com/kpi/gamificationtool/students/StudentForm.kt
+++ src/main/kotlin/com/kpi/gamificationtool/students/StudentForm.kt
@@ -1,8 +1,10 @@
 package com.kpi.gamificationtool.students
 
+import java.time.LocalDate
+
 data class StudentForm(
     val name: String,
-    val age: Int,
+    val birthDate: LocalDate,
     val login: String,
     val password: String,
     val groupId: Long,
